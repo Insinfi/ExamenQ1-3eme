7f4441f304c7ef5aefc390eecb1c046a139edfe1
Alexandre DECLERCQ <LA151079@student.helha.be>
2017-12-27 15:15:13 +0100

    grid

--- ExamenWPFprep/ExamenWPFprep/MainWindow.xaml.cs
+++ ExamenWPFprep/ExamenWPFprep/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,9 +21,33 @@ namespace ExamenWPFprep
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Timer RefreshHour;
         public MainWindow()
         {
             InitializeComponent();
+            this.Heure.Text = DateTime.Now.ToString("HH:MM:ss");
+            RefreshHour = new Timer(1000);
+            RefreshHour.Elapsed += RefreshHour_Elapsed;
+            RefreshHour.Enabled = true;
+            RefreshHour.Start();
+        }
+
+
+
+        void RefreshHour_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(() => { this.Heure.Text = DateTime.Now.Hour.ToString()+":"+ DateTime.Now.Minute.ToString()+":"+DateTime.Now.Second.ToString(); }));
+        }
+
+        private void Canvas_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Evenement au niveau du Canvas");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Evenement au niveau du bouton");
+            e.Handled = true;
         }
     }
 }
