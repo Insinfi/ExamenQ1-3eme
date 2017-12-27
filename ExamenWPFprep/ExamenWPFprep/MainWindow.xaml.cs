363d87ab558a5380f0cee7bac42670f331a56cb1
Alexandre DECLERCQ <LA151079@student.helha.be>
2017-12-27 18:45:36 +0100

    command

--- ExamenWPFprep/ExamenWPFprep/MainWindow.xaml.cs
+++ ExamenWPFprep/ExamenWPFprep/MainWindow.xaml.cs
@@ -25,29 +25,89 @@ namespace ExamenWPFprep
         public MainWindow()
         {
             InitializeComponent();
+
+
+            this.pgb.Alert += pgb_Alert;
+
+            //CommandBinding cmdBind = new CommandBinding();
+            //cmdBind.Command = ApplicationCommands.Close;
+            //cmdBind.Executed += new ExecutedRoutedEventHandler(CloseCommandExecute);
+            //this.CommandBindings.Add(cmdBind);
+
+            //COMMANDE TARGET
+            CommandManager.RegisterClassCommandBinding(typeof(MainWindow), new CommandBinding(ApplicationCommands.Close, CloseApplicationExecuted, CloseApplicationCanExecute));
+
+
             this.Heure.Text = DateTime.Now.ToString("HH:MM:ss");
             RefreshHour = new Timer(1000);
             RefreshHour.Elapsed += RefreshHour_Elapsed;
             RefreshHour.Enabled = true;
             RefreshHour.Start();
         }
+        //COMMANDE TARGET
 
+        private void CloseApplicationCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (this.Accept != null)
+            {
+                e.CanExecute = (bool)this.Accept.IsChecked;
+            }
+        }
 
+            //COMMANDE TARGET
+        private void CloseApplicationExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainWindow test = (MainWindow)sender;
+            test.Close();
+        }
 
         void RefreshHour_Elapsed(object sender, ElapsedEventArgs e)
         {
-            this.Dispatcher.BeginInvoke(new Action(() => { this.Heure.Text = DateTime.Now.Hour.ToString()+":"+ DateTime.Now.Minute.ToString()+":"+DateTime.Now.Second.ToString(); }));
+            this.Dispatcher.BeginInvoke(new Action(() => { this.Heure.Text = DateTime.Now.Hour.ToString()+":"+ DateTime.Now.Minute.ToString()+":"+DateTime.Now.Second.ToString(); if(pgb.Value<101) this.pgb.Value += 1; }));
         }
 
         private void Canvas_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Evenement au niveau du Canvas");
+            Window win = new Window1();
+            win.Show();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Evenement au niveau du bouton");
-            e.Handled = true;
+            //e.Handled = true; //empecher la propagation de l'event
+        }
+
+        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Evenement au niveau du bouton");
+
+        }
+
+        private void Canvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Evenement au niveau du Canvas");
+
+        }
+        void pgb_Alert(object sender,RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void pgb_Alert_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Alert");
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (this.Accept != null) e.CanExecute = (bool)this.Accept.IsChecked;
         }
     }
 }
