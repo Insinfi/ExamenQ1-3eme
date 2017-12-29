d54a0abe6a5988d328e431fd3377a526cbcbbaef
Alexandre DECLERCQ <LA151079@student.helha.be>
2017-12-29 17:22:58 +0100

    DataTemplateSelector

---
+++ ExamenWPFprep2/ExamenWPFprep2/UsersTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ExamenWPFprep2
+{
+    public class UsersTemplateSelector:DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var User = (GetAllUsersResult)item;
+            var window = Application.Current.MainWindow;
+            if ( User.Prenom == "Jean")
+            {
+                return (DataTemplate)window.FindResource("ComboItem2");
+            }
+            else
+            {
+                return (DataTemplate)window.FindResource("ComboItem");
+            }
+        }
+
+    }
+}
