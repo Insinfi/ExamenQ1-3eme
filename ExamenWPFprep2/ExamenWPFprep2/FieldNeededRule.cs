9441c922e9c6c0d2422318fa75b2784db4b82657
Alexandre DECLERCQ <LA151079@student.helha.be>
2017-12-29 17:50:36 +0100

    ValidationError

---
+++ ExamenWPFprep2/ExamenWPFprep2/FieldNeededRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ExamenWPFprep2
+{
+    class FieldNeededRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value.ToString().Length == 0)
+                return new ValidationResult(false, "Champ obligatoire");
+            else
+                return ValidationResult.ValidResult;
+        }
+    }
+}
