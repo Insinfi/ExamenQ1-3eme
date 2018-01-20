988c75fc934515ece9dce7a708ee6262ddb28f97
Allan KNOCKAERT <zedsciences12@gmail.com>
2018-01-20 18:09:12 +0100

    Fini pour aujourd'hui

---
+++ Web-Resto-Examen/ApiResto/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiResto.Controllers
+{
+    public class HomeController : ApiController
+    {
+        DataClasses1DataContext myContext = new DataClasses1DataContext();
+        // GET: Home
+        public IEnumerable<GetAllCategorieResult> GetAllCategorie()
+        {
+            var categories = myContext.GetAllCategorie().ToList();
+            return categories;
+        }
+    }
+}
